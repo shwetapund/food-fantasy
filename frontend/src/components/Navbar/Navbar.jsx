9db546dc59de2f13ef8bd8c77b30dcbb5410874d
shwetapund <pundshweta2020@gmail.com>
2024-05-12 02:29:13 -0700

    add to cart nad reomve from cart functionality is working

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} className='logo'/>
+   <Link to="/"> <img src={assets.logo} className='logo'/></Link>
       <ul className='navbar-menu'>
         <Link to="/" onClick={()=>setMenu("home")} className={menu==="home" ? "active":""}>home</Link>
         <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu" ? "active":""}>menu</a>
@@ -19,7 +19,7 @@ const Navbar = ({setShowLogin}) => {
       <div className='navbar-right'>
         <img src={assets.search_icon} />
         <div className='navbar-search-icon'>
-          <img src={assets.basket_icon} />
+          <Link to="/cart"><img src={assets.basket_icon} /></Link>
           <div className='dot'></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Sign in</button>
