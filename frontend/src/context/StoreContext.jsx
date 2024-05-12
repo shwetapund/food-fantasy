27b0fd7aa1c5e89a32a4bb392a5e96c3c6e29286
shwetapund <pundshweta2020@gmail.com>
2024-05-12 03:57:51 -0700

    successfully create get the totalAmount functionality

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,43 +1,50 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
-const StoreContextProvider = (props) =>{
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
 
-    const [cartItems, setCartItems] = useState({});
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 })); //value=1
+    } //this if state will crete a new entry for food product if that product id is not available in our cartItem then only
+    else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    } //if that product id is available in our cartItem then only this condition will increase the vlaue by 1
+  };
 
-    const addToCart = (itemId) =>{
-        if(!cartItems[itemId]){
-            setCartItems((prev) => ({...prev,[itemId]:1})) //value=1
-        } //this if state will crete a new entry for food product if that product id is not available in our cartItem then only 
-        else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-        } //if that product id is available in our cartItem then only this condition will increase the vlaue by 1
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  }; //this removefromcart functionality will decrease the value by one
+
+  const getTotalCartAmount = () => {
+    let totlAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totlAmount += itemInfo.price * cartItems[item];
+      }
     }
-    
-    const removeFromCart = (itemId)=>{
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-    } //this removefromcart functionality will decrease the value by one
+    return totlAmount;
+  }
 
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
-
-    const contextValue = {
-        food_list,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart
-    }
-    return(
-        <StoreContext.Provider value={contextValue}>
-        {props.children}
-        </StoreContext.Provider>
-    )
-}
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount
+  };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
 export { StoreContextProvider };
 
-export default StoreContextProvider
+export default StoreContextProvider;
