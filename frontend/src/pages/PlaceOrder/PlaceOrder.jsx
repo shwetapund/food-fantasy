c4cbf284eb1a3c9a9ef6ab9d1e81d5a43809df93
shwetapund <pundshweta2020@gmail.com>
2024-05-13 03:05:29 -0700

    now we have complete the frontend

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -30,19 +30,19 @@ const {getTotalCartAmount} = useContext(StoreContext)
       <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
-            <div className="cart-total-details">
+          <div className="cart-total-details">
               <p>Subtotal</p>
               <p>${getTotalCartAmount()}</p>
             </div>
             <hr/>
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr/>
             <div className="cart-total-details">
               <p>Total</p>
-              <p>${getTotalCartAmount()+2}</p>
+              <p>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</p>
             </div>
           </div>
 
