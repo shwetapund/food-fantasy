6e9528dc6d2b37e0b2d6f1e85d484ead6857ebe5
shwetapund <pundshweta2020@gmail.com>
2024-05-16 00:22:26 -0700

    we have created the Schema for food model

---
+++ backend/models/foodProduct.js
@@ -0,0 +1,24 @@
+import mongoose, {model, Schema} from "mongoose";
+
+const foodSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    }
+})
