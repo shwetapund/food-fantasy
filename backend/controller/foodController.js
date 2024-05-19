f7db0430c31e0ad1911077b44d84d635ac6382e1
shwetapund <pundshweta2020@gmail.com>
2024-05-19 01:52:14 -0700

    create the post api for added food

--- backend/controller/foodController.js
+++ backend/controller/foodController.js
@@ -4,6 +4,25 @@ import fs from "fs" //using this FS file system that is pre-built in the nodejs
 //add food item 
 
 const addFood = async(req,res)=>{
-    
+    let image_filename = `${req.file.filename}`;
+
+    const food = new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.file,
+        image:image_filename
+    })
+    try{
+        await food.save();
+        res.json({success:true,message:"food added"})
+    } catch(error){
+        console.log(error)
+        res.json({
+            success:false,
+            message:"Error"
+        })
+    }
 }
 export { addFood }
+
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addFood } from "../controller/foodController";
+import { addFood } from "../controller/foodController.js";
 import multer from "multer"; //using that we will create the image storage system
 
 const foodRouter = express.Router(); //using this router we can create the get method post method and other many
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage}) //2nd storage create kiya using the disk storage
 
-foodRouter.post("/add",addFood) //use the post method to send the data on the server
+foodRouter.post("/add",upload.single('image'),addFood) //use the post method to send the data on the server
 
 //upload middleawre:- using that we can store the image in the upload folder
 
