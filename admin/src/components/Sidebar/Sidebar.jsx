b4f09bcbe1cbf6b268afbd0ecb1de05e06b627d8
shwetapund <pundshweta2020@gmail.com>
2024-07-30 03:28:04 -0700

    create sidebar for admin

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -7,7 +7,16 @@ const Sidebar = () => {
     <div className='sidebar'>
         <div className="sidebar-options">
           <div className='sidebar-option'>
-         
+            <img src={assets.add_icon} alt=''/>
+            <p>Add Items</p>
+          </div>
+          <div className='sidebar-option'>
+            <img src={assets.order_icon} alt=''/>
+            <p>List Items</p>
+          </div>
+          <div className='sidebar-option'>
+            <img src={assets.add_icon} alt=''/>
+            <p>Orders</p>
           </div>
         </div>
     </div>
